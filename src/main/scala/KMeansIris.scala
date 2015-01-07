60dd7ea1dedaf1674cf6d9136ee474396ccf6ed3
ymmtmdk <ymmtmdk@gmail.com>
2015-01-07 23:00:46 +0900

    a

--- src/main/scala/KMeansIris.scala
+++ src/main/scala/KMeansIris.scala
@@ -8,13 +8,12 @@ object KMeansIris extends App {
   val context = new SparkContext("local", "demo")
 
   val data = context.
-  textFile("iris.data").
-  map { s =>
-    val elems = s.split(",")
-    
-    println(elems.last, elems.init.map(_.toDouble).getClass(), Vectors.dense(elems.init.map(_.toDouble)))
-    (elems.last, Vectors.dense(elems.init.map(_.toDouble)))
-  }
+    textFile("iris.data").
+    map { s =>
+      val elems = s.split(",")
+      println(elems.last, elems.init.map(_.toDouble).getClass(), Vectors.dense(elems.init.map(_.toDouble)))
+      (elems.last, Vectors.dense(elems.init.map(_.toDouble)))
+    }
 
   val k = 3 // クラスタの個数を指定します
   val maxItreations = 100 // K-means のイテレーション最大回数を指定します
@@ -30,6 +29,6 @@ object KMeansIris extends App {
   println("## 各データのクラスタリング結果")
   data.foreach { tuple =>
     println(f"${tuple._2.toArray.mkString("[", ", ", "]")}%s " +
-            f"(${tuple._1}%s) : cluster = ${clusters.predict(tuple._2)}%d")
+      f"(${tuple._1}%s) : cluster = ${clusters.predict(tuple._2)}%d")
   }
 }
